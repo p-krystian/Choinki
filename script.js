b834a35414714cfb40056b37ed9c4c72cfe220ce
Krystian Piątek <piatekkrystian13@gmail.com>
2026-03-01 11:14:11 +0100

    Add auto sizing-down logic

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const info = document.querySelector('.info');
 const action = (innerHeight < innerWidth) ? 'input' : 'change';
 let aV, bV, cV, dV, eV;
 let currentTree;
+
 // Functions
 function apply(){
   aV = +rangeA.value;
@@ -19,13 +20,38 @@ function apply(){
   currentTree = `${tree(aV, bV, cV, dV, eV)}`;
   result.innerHTML = currentTree;
 }
-function checking(){
-  while (+rangeA.value < +rangeB.value) rangeA.valueAsNumber += 2;
-  while (+rangeA.value > +rangeC.value) rangeC.valueAsNumber += 2;
-  while (+rangeC.value < +rangeB.value) rangeC.valueAsNumber += 2;
-  while (+rangeE.value >= +rangeC.value) rangeE.valueAsNumber -= 2;
+
+function checking(changedElement){
+  let a = +rangeA.value;
+  let b = +rangeB.value;
+  let c = +rangeC.value;
+  let e = +rangeE.value;
+
+  switch(changedElement){
+    case 'a':
+      if (a < b) rangeB.valueAsNumber = a;
+      if (a > c) rangeC.valueAsNumber = a;
+      break;
+
+    case 'b':
+      if (b > a) rangeA.valueAsNumber = b;
+      if (b > c) rangeC.valueAsNumber = b;
+      break;
+
+    case 'c':
+      if (c < a) rangeA.valueAsNumber = c;
+      if (c < b) rangeB.valueAsNumber = c;
+      if (c <= e) rangeE.valueAsNumber = Math.max(1, c - 2);
+      break;
+
+    case 'e':
+      if (e >= c) rangeC.valueAsNumber = e + 2;
+      break;
+  }
+
   apply();
 }
+
 function tree(a, b, c, d, e){
   let chTree = '';
   for (let i = 1; i <= a; i += 2) chTree += `${createLine(c, i)}\n`;
@@ -33,6 +59,7 @@ function tree(a, b, c, d, e){
   for (let i = 0; i < d; i++) chTree += `${createLine(c, e)}\n`;
   return chTree;
 }
+
 function createLine(lineLenght, hashCount){
   let dotCount = (lineLenght - hashCount) / 2;
   let line = '';
@@ -41,16 +68,18 @@ function createLine(lineLenght, hashCount){
   for (let i = 0; i < dotCount; i++) line += '.';
   return line;
 }
+
 function copy(){
   result.select();
   document.execCommand('copy');
   info.style.animationName = 'eject';
   setTimeout(() => (info.style.animationName = ''), 3000);
 }
+
 // Run
 window.addEventListener('load', () => setTimeout(apply, 100));
-rangeA.addEventListener(action, checking);
-rangeB.addEventListener(action, checking);
-rangeC.addEventListener(action, checking);
-rangeD.addEventListener(action, checking);
-rangeE.addEventListener(action, checking);
+rangeA.addEventListener(action, () => checking('a'));
+rangeB.addEventListener(action, () => checking('b'));
+rangeC.addEventListener(action, () => checking('c'));
+rangeD.addEventListener(action, () => checking('d'));
+rangeE.addEventListener(action, () => checking('e'));
